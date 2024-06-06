ae6aa1d77888bbf1baaa4b41e2181859f70b9357
IlyaKir <ilya.kirillov.vic@gmail.com>
2024-06-06 11:02:17 +0200

    tests bug fix

--- src/main/scala/prewave/matcher/Matcher.scala
+++ src/main/scala/prewave/matcher/Matcher.scala
@@ -2,6 +2,7 @@ package prewave.matcher
 
 import prewave.model.AlertsMatchesResult
 
+// TODO Result type must depend on `T`, `V`
 trait Matcher[T, V] {
   def `match`(terms: Seq[T], alerts: Seq[V]): AlertsMatchesResult
 }
--- src/test/scala/prewave/matcher/MatcherViaRegexSpec.scala
+++ src/test/scala/prewave/matcher/MatcherViaRegexSpec.scala
@@ -9,33 +9,33 @@ class MatcherViaRegexSpec extends AnyFlatSpec with Matchers {
   private val keepOrderedPath = "src/test/files/keepOrdered"
   private val notKeepOrderedPath = "src/test/files/notKeepOrdered"
 
-  private lazy val koTerm = JsonGetter.get[QueryTerm](s"$keepOrderedPath/term.json")
-  private lazy val nkoTerm = JsonGetter.get[QueryTerm](s"$notKeepOrderedPath/term.json")
+  private lazy val keepOrderedTerm = JsonGetter.get[QueryTerm](s"$keepOrderedPath/term.json")
+  private lazy val nonKeepOrderedTerm = JsonGetter.get[QueryTerm](s"$notKeepOrderedPath/term.json")
 
   "keepOrdered term" should "match" in {
     val alerts = JsonGetter.get[Alert](s"$keepOrderedPath/alertsMatch.json")
-    val result = MatcherViaRegex.`match`(koTerm, alerts)
+    val result = MatcherViaRegex.`match`(keepOrderedTerm, alerts)
 
     result.matches.head.alerts.length shouldBe alerts.length
   }
 
   it should "not match" in {
     val alerts = JsonGetter.get[Alert](s"$keepOrderedPath/alertsNotMatch.json")
-    val result = MatcherViaRegex.`match`(koTerm, alerts)
+    val result = MatcherViaRegex.`match`(keepOrderedTerm, alerts)
 
     result.matches.length shouldBe 0
   }
 
   "not keepOrdered term" should "match" in {
     val alerts = JsonGetter.get[Alert](s"$notKeepOrderedPath/alertsMatch.json")
-    val result = MatcherViaRegex.`match`(nkoTerm, alerts)
+    val result = MatcherViaRegex.`match`(nonKeepOrderedTerm, alerts)
 
     result.matches.head.alerts.length shouldBe alerts.length
   }
 
   it should "not match" in {
     val alerts = JsonGetter.get[Alert](s"$notKeepOrderedPath/alertsNotMatch.json")
-    val result = MatcherViaRegex.`match`(nkoTerm, alerts)
+    val result = MatcherViaRegex.`match`(nonKeepOrderedTerm, alerts)
 
     result.matches.length shouldBe 0
   }
